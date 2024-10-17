1e49517e1590d8d5cd989cdecbf67266636ea9ae
Naomi <naomismith97@yahoo.co.uk>
2024-10-17 12:23:17 +0100

    completed recipe class and tests

--- lib/recipe.py
+++ lib/recipe.py
@@ -1,2 +1,12 @@
 class Recipe():
-    pass
+    def __init__(self, id, title, average_cooking_time, rating):
+        self.id = id
+        self.title = title
+        self.average_cooking_time = average_cooking_time
+        self.rating = rating
+    
+    def __repr__(self):
+        return f"Recipe({self.id}, {self.title}, {self.average_cooking_time} minutes, {self.rating} stars)"
+    
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -1,2 +1,17 @@
 from lib.recipe import Recipe
 
+def test_recipe_constructs():
+    recipe = Recipe(1, "Choc Cookies", 60, 5)
+    assert recipe.id == 1
+    assert recipe.title == "Choc Cookies"
+    assert recipe.average_cooking_time == 60
+    assert recipe.rating == 5
+
+def test_recipe_formats_correctly():
+    recipe = Recipe(2, "Ice Cream Float", 5, 4)
+    assert recipe.__repr__() == "Recipe(2, Ice Cream Float, 5 minutes, 4 stars)"
+
+def test_recipes_are_equal():
+    recipe1 = Recipe(5, "Boiled Snake", 45, 3)
+    recipe2 = Recipe(5, "Boiled Snake", 45, 3)
+    assert recipe1 == recipe2
