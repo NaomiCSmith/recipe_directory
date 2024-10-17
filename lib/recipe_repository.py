d468d5f7dfe634e59972c98b6ff2417022bcbe99
Naomi <naomismith97@yahoo.co.uk>
2024-10-17 14:07:31 +0100

    recipe repository class, all and find functions, and tests

--- lib/recipe_repository.py
+++ lib/recipe_repository.py
@@ -1,2 +1,18 @@
+from lib.recipe import Recipe
+
 class RecipeRepository():
-    pass
+    def __init__(self, _connection):
+        self._connection = _connection
+    
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM recipes')
+        recipes_list = []
+        for row in rows:
+            item = Recipe(row["id"], row["title"], row["average_cooking_time"], row["rating"])
+            recipes_list.append(item)
+        return recipes_list
+    
+    def find(self, id):
+        rows = self._connection.execute('SELECT * FROM recipes WHERE id = %s', [id])
+        row = rows[0]
+        return Recipe(row["id"], row["title"], row["average_cooking_time"], row["rating"])
--- tests/test_recipe_repository.py
+++ tests/test_recipe_repository.py
@@ -1,2 +1,20 @@
 from lib.recipe_repository import RecipeRepository
 from lib.recipe import Recipe
+
+
+def test_get_all_recipes(db_connection):
+    db_connection.seed("seeds/recipe_directory.sql")
+    recipe = RecipeRepository(db_connection)
+    assert recipe.all() == [
+        Recipe(1, 'Spagetti Bolognese', 90, 4),
+        Recipe(2, 'Packet Ramen', 5, 3),
+        Recipe(3, 'Smoked Rat with Rosemary Potatoes', 60, 1),
+        Recipe(4, 'Chicken Kebab', 60, 4),
+        Recipe(5, 'Chilli con Carne', 120, 5)
+    ]
+
+def test_finds_single_recipe(db_connection):
+    db_connection.seed("seeds/recipe_directory.sql")
+    recipe = RecipeRepository(db_connection)
+    assert recipe.find(3) == Recipe(3, 'Smoked Rat with Rosemary Potatoes', 60, 1)
+
